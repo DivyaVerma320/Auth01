a2db7a1df8491088ae5df2e5e912a6aea9830d83
DivyaVerma320 <divyaver1967@gmail.com>
2022-07-05 11:02:39 +0530

    Initial Commit

---
+++ src/strore/index.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import counterReducer from './counterSlice';
+import authReducer from './authSlice';
+
+const store = configureStore({
+    reducer: { counters: counterReducer, auth: authReducer }
+});
+
+export default store;
